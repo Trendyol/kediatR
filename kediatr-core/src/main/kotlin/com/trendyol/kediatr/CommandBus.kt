934a5517838f7665d874e9ba10484b5503cce8c2
Caner Patır <caner.patir@trendyol.com>
2020-02-20 10:38:19 +0300

    async handler support simplified without opening new coroutine scope

--- kediatr-core/src/main/kotlin/com/trendyol/kediatr/CommandBus.kt
+++ kediatr-core/src/main/kotlin/com/trendyol/kediatr/CommandBus.kt
@@ -7,11 +7,8 @@ import kotlinx.coroutines.Job
 
 interface CommandBus {
     fun <TCommand : Command> executeCommand(command: TCommand)
-
     fun <R, Q : Query<R>> executeQuery(query: Q): R
-
-    suspend fun <TCommand : Command> executeCommandAsync(command: TCommand, dispatcher: CoroutineDispatcher = Dispatchers.IO)
-
-    suspend fun <R, Q : Query<R>> executeQueryAsync(query: Q, dispatcher: CoroutineDispatcher = Dispatchers.IO): R
+    suspend fun <TCommand : Command> executeCommandAsync(command: TCommand)
+    suspend fun <R, Q : Query<R>> executeQueryAsync(query: Q): R
 }
 
--- kediatr-core/src/main/kotlin/com/trendyol/kediatr/CommandBusImpl.kt
+++ kediatr-core/src/main/kotlin/com/trendyol/kediatr/CommandBusImpl.kt
@@ -1,23 +1,11 @@
 package com.trendyol.kediatr
 
-import kotlinx.coroutines.CoroutineDispatcher
-import kotlinx.coroutines.coroutineScope
-import kotlinx.coroutines.withContext
-
 class CommandBusImpl(private val registry: RegistryImpl) : CommandBus {
-    override suspend fun <TCommand : Command> executeCommandAsync(command: TCommand, dispatcher: CoroutineDispatcher) =
-        coroutineScope {
-            withContext(dispatcher) {
-                registry.resolveAsyncCommandHandler(command.javaClass).handleAsync(command)
-            }
-        }
+    override suspend fun <TCommand : Command> executeCommandAsync(command: TCommand) =
+        registry.resolveAsyncCommandHandler(command.javaClass).handleAsync(command)
 
-    override suspend fun <R, Q : Query<R>> executeQueryAsync(query: Q, dispatcher: CoroutineDispatcher): R =
-        coroutineScope {
-            withContext(dispatcher) {
-                registry.resolveAsyncQueryHandler(query.javaClass).handleAsync(query)
-            }
-        }
+    override suspend fun <R, Q : Query<R>> executeQueryAsync(query: Q): R =
+        registry.resolveAsyncQueryHandler(query.javaClass).handleAsync(query)
 
     override fun <TCommand : Command> executeCommand(command: TCommand) =
         registry.resolveCommandHandler(command.javaClass).handle(command)
