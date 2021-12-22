3d2eb9acac6653bb1b555447a2a8325b530e5f3b
mehmet.sezer <mehmet.sezer@trendyol.com>
2021-12-22 08:36:38 +0300

    Performance improvements for finding all initial kediatr beans

--- kediatr-quarkus-starter/src/main/kotlin/com/trendyol/KediatrBeanProvider.kt
+++ kediatr-quarkus-starter/src/main/kotlin/com/trendyol/KediatrBeanProvider.kt
@@ -12,9 +12,10 @@ import javax.enterprise.inject.spi.BeanManager
 class KediatrBeanProvider(
     private val beanManager: BeanManager
 ) : DependencyProvider {
-    private val classHandlerMap: ConcurrentHashMap<Class<Any>, Any> = ConcurrentHashMap()
 
-    private var initialBeans: Set<Bean<*>> = emptySet()
+    private val KEDIATR_PACKAGE_PREFIX = "com.trendyol.kediatr"
+    private val classHandlerMap: ConcurrentHashMap<Class<Any>, Any> = ConcurrentHashMap()
+    private var initialAllKediatrBeans: List<Bean<*>> = emptyList()
 
     override fun <T> getSingleInstanceOf(clazz: Class<T>): T {
         val instance = classHandlerMap.getOrPut(clazz as Class<Any>) {
@@ -27,16 +28,20 @@ class KediatrBeanProvider(
     }
 
     override fun <T> getSubTypesOf(clazz: Class<T>): Collection<Class<T>> {
-        if (initialBeans.isEmpty()) {
-            initialBeans = beanManager.getBeans(Object::class.java)
+        if (initialAllKediatrBeans.isEmpty()) {
+            initialAllKediatrBeans = beanManager.getBeans(Object::class.java).filter { bean ->
+                bean.types.any { type ->
+                    type.typeName.startsWith(KEDIATR_PACKAGE_PREFIX)
+                }
+            }
         }
 
         val kediatrHandlerBeans: MutableList<Class<T>> = mutableListOf()
-        initialBeans.forEach { bean ->
-            val isKediatrBean = bean.types.any { type ->
+        initialAllKediatrBeans.forEach { bean ->
+            val isKediatrHandlerBean = bean.types.any { type ->
                 type.typeName.startsWith(clazz.name)
             }
-            if (isKediatrBean) {
+            if (isKediatrHandlerBean) {
                 val kediatrHandler =
                     bean.types.first { !it.typeName.startsWith(clazz.name) && !it.typeName.equals(Object::class.java.name) }
                 kediatrHandlerBeans.add(kediatrHandler as Class<T>)
