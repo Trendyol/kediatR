0947e59c411098485773982697b61085d8589f6e
Oguzhan Soykan <oguzhansoykan@gmail.com>
2024-10-30 11:38:15 +0100

    disable testFixtures publishing

--- projects/kediatr-core/build.gradle.kts
+++ projects/kediatr-core/build.gradle.kts
@@ -16,3 +16,7 @@ dependencies {
 kotlin {
   jvmToolchain(11)
 }
+
+val javaComponent = components["java"] as AdhocComponentWithVariants
+javaComponent.withVariantsFromConfiguration(configurations["testFixturesApiElements"]) { skip() }
+javaComponent.withVariantsFromConfiguration(configurations["testFixturesRuntimeElements"]) { skip() }
