bcf92f782b1db9bd591c52d91e7e332f2253842c
Oguzhan Soykan <oguzhansoykan@gmail.com>
2024-10-30 13:15:30 +0100

    Cache pipeline behaviours on mediatorImpl

--- projects/kediatr-core/src/main/kotlin/com/trendyol/kediatr/MediatorImpl.kt
+++ projects/kediatr-core/src/main/kotlin/com/trendyol/kediatr/MediatorImpl.kt
@@ -4,50 +4,30 @@ class MediatorImpl(
   private val registry: Registry,
   private val defaultPublishStrategy: PublishStrategy = StopOnExceptionPublishStrategy()
 ) : Mediator {
-  override suspend fun <TQuery : Query<TResponse>, TResponse> send(query: TQuery): TResponse =
-    processPipeline(
-      registry.getPipelineBehaviors(),
-      query
-    ) {
-      registry.resolveQueryHandler(query.javaClass).handle(query)
-    }
+  private val sortedPipelineBehaviors by lazy { registry.getPipelineBehaviors().sortedByDescending { it.order } }
 
-  override suspend fun <TCommand : Command> send(command: TCommand) =
-    processPipeline(
-      registry.getPipelineBehaviors(),
-      command
-    ) {
-      registry.resolveCommandHandler(command.javaClass).handle(command)
-    }
+  override suspend fun <TQuery : Query<TResponse>, TResponse> send(
+    query: TQuery
+  ): TResponse = handle(query) { registry.resolveQueryHandler(query.javaClass).handle(query) }
 
-  override suspend fun <TCommand : CommandWithResult<TResult>, TResult> send(command: TCommand): TResult =
-    processPipeline(
-      registry.getPipelineBehaviors(),
-      command
-    ) {
-      registry.resolveCommandWithResultHandler(command.javaClass).handle(command)
-    }
+  override suspend fun <TCommand : Command> send(
+    command: TCommand
+  ) = handle(command) { registry.resolveCommandHandler(command.javaClass).handle(command) }
+
+  override suspend fun <TCommand : CommandWithResult<TResult>, TResult> send(
+    command: TCommand
+  ): TResult = handle(command) { registry.resolveCommandWithResultHandler(command.javaClass).handle(command) }
 
   override suspend fun <T : Notification> publish(notification: T) = publish(notification, defaultPublishStrategy)
 
   override suspend fun <T : Notification> publish(
     notification: T,
     publishStrategy: PublishStrategy
-  ) = processPipeline(
-    registry.getPipelineBehaviors(),
-    notification
-  ) {
-    publishStrategy.publish(notification, registry.resolveNotificationHandlers(notification.javaClass))
-  }
+  ) = handle(notification) { publishStrategy.publish(notification, registry.resolveNotificationHandlers(notification.javaClass)) }
 
-  private suspend fun <TRequest, TResponse> processPipeline(
-    pipelineBehaviors: Collection<PipelineBehavior>,
+  private suspend fun <TRequest, TResponse> handle(
     request: TRequest,
     handler: RequestHandlerDelegate<TRequest, TResponse>
-  ): TResponse =
-    pipelineBehaviors
-      .sortedByDescending { it.order }
-      .fold(handler) { next, pipeline ->
-        { pipeline.handle(request) { next(it) } }
-      }(request)
+  ): TResponse = sortedPipelineBehaviors
+    .fold(handler) { next, pipeline -> { pipeline.handle(request) { next(it) } } }(request)
 }
