217b4b9f995b519e5fbb83412de91d5643c50dbd
Geir Sagberg <geir.sagberg@gmail.com>
2025-02-10 12:47:49 +0100

    Make spring beans conditional (#418)
    * Make spring beans conditional
    
    Allows overriding the registration of beans, for customization
    
    * apply spotless
    
    * missing imports
    
    ---------
    
    Co-authored-by: Oguzhan Soykan <oguzhansoykan@gmail.com>

--- projects/kediatr-spring-boot-3x-starter/src/main/kotlin/com/trendyol/kediatr/spring/KediatRAutoConfiguration.kt
+++ projects/kediatr-spring-boot-3x-starter/src/main/kotlin/com/trendyol/kediatr/spring/KediatRAutoConfiguration.kt
@@ -2,17 +2,20 @@ package com.trendyol.kediatr.spring
 
 import com.trendyol.kediatr.*
 import org.springframework.boot.autoconfigure.AutoConfiguration
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.context.ApplicationContext
 import org.springframework.context.annotation.Bean
 
 @AutoConfiguration
 open class KediatRAutoConfiguration {
   @Bean
+  @ConditionalOnMissingBean
   open fun kediatRSpringBeanProvider(
     applicationContext: ApplicationContext
   ): KediatRSpringBeanProvider = KediatRSpringBeanProvider(applicationContext)
 
   @Bean
+  @ConditionalOnMissingBean
   open fun mediator(
     kediatRSpringBeanProvider: KediatRSpringBeanProvider
   ): Mediator = MediatorBuilder(kediatRSpringBeanProvider).build()
