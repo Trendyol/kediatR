eff4eaa9285ad4bcf924aeb4850157db8984599e
Oguzhan Soykan <oguzhansoykan@gmail.com>
2025-06-04 14:01:39 +0300

    skip testFixturesSourcesElements on gradle publish

--- projects/kediatr-core/build.gradle.kts
+++ projects/kediatr-core/build.gradle.kts
@@ -21,3 +21,6 @@ kotlin {
 val javaComponent = components["java"] as AdhocComponentWithVariants
 javaComponent.withVariantsFromConfiguration(configurations["testFixturesApiElements"]) { skip() }
 javaComponent.withVariantsFromConfiguration(configurations["testFixturesRuntimeElements"]) { skip() }
+afterEvaluate {
+  javaComponent.withVariantsFromConfiguration(configurations["testFixturesSourcesElements"]) { skip() }
+}
