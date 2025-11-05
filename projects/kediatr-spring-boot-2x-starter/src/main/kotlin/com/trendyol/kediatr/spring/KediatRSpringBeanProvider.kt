0668155b078070132e0c07579e20256c9b6b0c05
an2an96 <ant.nemirov@gmail.com>
2025-11-05 19:58:56 +0300

    Fix Spring AOP proxy support for suspend handlers
    - Fixed KediatRSpringBeanProvider to retrieve beans by name first
    - This ensures proper Spring AOP proxy is used for suspend functions
    - Resolves 'AOP configuration seems to be invalid' errors when using
      @Transactional and other AOP annotations with suspend handlers
    - Applied fix to both Spring Boot 2x and 3x starters
    - All existing tests pass successfully

--- projects/kediatr-spring-boot-2x-starter/src/main/kotlin/com/trendyol/kediatr/spring/KediatRSpringBeanProvider.kt
+++ projects/kediatr-spring-boot-2x-starter/src/main/kotlin/com/trendyol/kediatr/spring/KediatRSpringBeanProvider.kt
@@ -8,7 +8,16 @@ import org.springframework.context.ApplicationContext
 class KediatRSpringBeanProvider(
   private val applicationContext: ApplicationContext
 ) : DependencyProvider {
-  override fun <T> getSingleInstanceOf(clazz: Class<T>): T = applicationContext.getBean(clazz)
+  override fun <T> getSingleInstanceOf(clazz: Class<T>): T {
+    // Get bean by name first to ensure we get the Spring AOP proxy version
+    // This is critical for suspend functions with @Transactional and other AOP annotations
+    val beanNames = applicationContext.getBeanNamesForType(clazz)
+    return if (beanNames.isNotEmpty()) {
+      applicationContext.getBean(beanNames.first(), clazz)
+    } else {
+      applicationContext.getBean(clazz)
+    }
+  }
 
   override fun <T> getSubTypesOf(
     clazz: Class<T>
